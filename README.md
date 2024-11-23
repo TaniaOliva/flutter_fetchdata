0aff81e4bf9c8cea1bfdc5b88079d2d19d6573bb
TaniaOliva <taniaoliva1214@gmail.com>
2024-11-23 02:38:08 -0600

    primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_fetchdata
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_fetchdata/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_fetchdata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
